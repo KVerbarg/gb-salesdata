7d815e07ffe7f60b588d37e7fdc4e52f6afb0c26
Knut Verbarg <knut.verbarg@hochschule-stralsund.de>
2022-12-25 17:23:21 +0100

    credentials

--- abap/Credentials.py
+++ abap/Credentials.py
@@ -3,3 +3,7 @@ p_host = 'https://'
 p_client = 200
 p_username = ''
 p_password = ''
+
+# I used
+# git update-index --assume-unchanged .\abap\Credentials.py
+# to stop git from tracking changes without changing remote (on github)
